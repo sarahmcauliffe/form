994e5eb7331f8f696da5f192faf96b85d6391b05
unknown <sarahmac287@hotmail.com>
2021-11-03 00:06:53 +0000

    added index,css and style sheet

---
+++ style.js
@@ -0,0 +1,9 @@
+function alter(){
+	document.getElementById("form").submit(); 
+	event.preventDefault();
+}
+
+
+//    document.getElementById("confirm").event.preventDefault();
+   // document.getElementById("form").event.preventDefault();
+    
